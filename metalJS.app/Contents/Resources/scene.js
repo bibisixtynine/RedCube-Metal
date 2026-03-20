c95ce9a513ea46029cab4c1b96170ace3e07dc27
ilboued <ilboued@me.com>
2026-03-20 13:20:56 +0100

    Update default scene.js to match new README loop

--- metalJS.app/Contents/Resources/scene.js
+++ metalJS.app/Contents/Resources/scene.js
@@ -1,16 +1,11 @@
 // scene.js
 console.log("Starting JS scene...");
 
-// Draw cubes at various positions with varied sizes
-drawCube(-1.5, 1.0, 0.5);
-drawCube(0.0, 1.5, 0.8);
-drawCube(1.5, 1.0, 0.4);
-
-drawCube(-2.0, -1.0, 0.6);
-drawCube(0.0, -1.5, 1.2);
-drawCube(2.0, -1.0, 0.7);
-
-drawCube(-3.0, 0.0, 0.3);
-drawCube(3.0, 0.0, 0.3);
+// A simple grid of cubes
+for (let x = -2; x <= 2; x=x+0.1) {
+    for (let y = -2; y <= 2; y=y+0.1) {
+        drawCube(x, y, 0.3);
+    }
+}
 
 console.log("Cubes registered!");
